f0008457d87655fb2f9ab5ca09cfef61e6307b72
nelsoncvjr <ircnelson@gmail.com>
2012-12-19 13:41:22 -0300

    limpando sujeira

---
+++ ExampleSignalR/Highcharts/HighchartsBasic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExampleSignalR.Highcharts
+{
+    public class HighchartsBasic<T>
+    {
+        public string Name { get; set; }
+        public virtual IList<T> Data { get; set; }
+    }
+}
